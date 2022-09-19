7166ca1b714db783716a8a67b1c7cffc274f2f84
tkaven <4628730+tkaven@users.noreply.github.com>
2022-09-19 14:34:43 +0800

    Update AbstractModel.php

--- src/TencentCloud/Common/AbstractModel.php
+++ src/TencentCloud/Common/AbstractModel.php
@@ -26,7 +26,7 @@ use \ReflectionClass;
 abstract class AbstractModel
 {
 
-    public static function newStatic()
+    public static function newInstance()
     {
         return new static();
     }

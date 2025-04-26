30c2de352047d2ef5b6461ee5809adfd4c7b954d
tencentcloudapi <tencentcloudapi@tencent.com>
2025-04-26 11:37:35 +0800

    release 3.0.1368

--- src/TencentCloud/Ocr/V20181119/Models/SmartStructuralProRequest.php
+++ src/TencentCloud/Ocr/V20181119/Models/SmartStructuralProRequest.php
@@ -24,10 +24,16 @@ use TencentCloud\Common\AbstractModel;
  * @method void setImageUrl(string $ImageUrl) 设置图片的 Url 地址。支持的图片格式：PNG、JPG、JPEG，WORD，EXCEL，暂不支持 GIF 格式。支持的图片大小：所下载图片经 Base64 编码后不超过 7M。图片下载时间不超过 3 秒。支持的图片像素：需介于20-10000px之间。图片存储于腾讯云的 Url 可保障更高的下载速度和稳定性，建议图片存储于腾讯云。非腾讯云存储的 Url 速度和稳定性可能受一定影响。
  * @method string getImageBase64() 获取图片的 Base64 值。支持的图片格式：PNG、JPG、JPEG，WORD，EXCEL，暂不支持 GIF 格式。支持的图片大小：所下载图片经Base64编码后不超过 7M。图片下载时间不超过 3 秒。支持的图片像素：需介于20-10000px之间。图片的 ImageUrl、ImageBase64 必须提供一个，如果都提供，只使用 ImageUrl。
  * @method void setImageBase64(string $ImageBase64) 设置图片的 Base64 值。支持的图片格式：PNG、JPG、JPEG，WORD，EXCEL，暂不支持 GIF 格式。支持的图片大小：所下载图片经Base64编码后不超过 7M。图片下载时间不超过 3 秒。支持的图片像素：需介于20-10000px之间。图片的 ImageUrl、ImageBase64 必须提供一个，如果都提供，只使用 ImageUrl。
- * @method integer getPdfPageNumber() 获取需要识别的PDF页面的对应页码，仅支持PDF单页识别，当上传文件为PDF且IsPdf参数值为true时有效，默认值为1。
- * @method void setPdfPageNumber(integer $PdfPageNumber) 设置需要识别的PDF页面的对应页码，仅支持PDF单页识别，当上传文件为PDF且IsPdf参数值为true时有效，默认值为1。
+ * @method integer getPdfPageNumber() 获取需要识别的PDF页面的对应页码，仅支持PDF单页识别，当上传文件为PDF且IsPdf参数值为true时有效，默认值为前3页。
+ * @method void setPdfPageNumber(integer $PdfPageNumber) 设置需要识别的PDF页面的对应页码，仅支持PDF单页识别，当上传文件为PDF且IsPdf参数值为true时有效，默认值为前3页。
  * @method array getItemNames() 获取自定义结构化功能需返回的字段名称，例：若客户想新增返回姓名、性别两个字段的识别结果，则输入ItemNames=["姓名","性别"]
  * @method void setItemNames(array $ItemNames) 设置自定义结构化功能需返回的字段名称，例：若客户想新增返回姓名、性别两个字段的识别结果，则输入ItemNames=["姓名","性别"]
+ * @method boolean getItemNamesShowMode() 获取true：仅输出自定义字段
+flase：输出默认字段+自定义字段
+默认true
+ * @method void setItemNamesShowMode(boolean $ItemNamesShowMode) 设置true：仅输出自定义字段
+flase：输出默认字段+自定义字段
+默认true
  * @method boolean getReturnFullText() 获取是否开启全文字段识别
  * @method void setReturnFullText(boolean $ReturnFullText) 设置是否开启全文字段识别
  * @method string getConfigId() 获取配置id支持：
@@ -46,6 +52,7 @@ AirWayBill -- 航空运单识别模板
 DispatchWeightNote -- 磅单发货单识别模板
 ReceiptWeightNote -- 磅单收货单识别模板
 ArticalRecognize -- 手写作文模版
+Table -- 表格模版
  * @method void setConfigId(string $ConfigId) 设置配置id支持：
 General -- 通用场景 
 InvoiceEng -- 国际invoice模版 
@@ -62,6 +69,7 @@ AirWayBill -- 航空运单识别模板
 DispatchWeightNote -- 磅单发货单识别模板
 ReceiptWeightNote -- 磅单收货单识别模板
 ArticalRecognize -- 手写作文模版
+Table -- 表格模版
  * @method boolean getEnableCoord() 获取是否开启全文字段坐标值的识别
  * @method void setEnableCoord(boolean $EnableCoord) 设置是否开启全文字段坐标值的识别
  * @method boolean getOutputParentKey() 获取是否开启父子key识别，默认是
@@ -82,7 +90,7 @@ class SmartStructuralProRequest extends AbstractModel
     public $ImageBase64;
 
     /**
-     * @var integer 需要识别的PDF页面的对应页码，仅支持PDF单页识别，当上传文件为PDF且IsPdf参数值为true时有效，默认值为1。
+     * @var integer 需要识别的PDF页面的对应页码，仅支持PDF单页识别，当上传文件为PDF且IsPdf参数值为true时有效，默认值为前3页。
      */
     public $PdfPageNumber;
 
@@ -90,6 +98,13 @@ class SmartStructuralProRequest extends AbstractModel
      * @var array 自定义结构化功能需返回的字段名称，例：若客户想新增返回姓名、性别两个字段的识别结果，则输入ItemNames=["姓名","性别"]
      */
     public $ItemNames;
+
+    /**
+     * @var boolean true：仅输出自定义字段
+flase：输出默认字段+自定义字段
+默认true
+     */
+    public $ItemNamesShowMode;
 
     /**
      * @var boolean 是否开启全文字段识别
@@ -113,6 +128,7 @@ AirWayBill -- 航空运单识别模板
 DispatchWeightNote -- 磅单发货单识别模板
 ReceiptWeightNote -- 磅单收货单识别模板
 ArticalRecognize -- 手写作文模版
+Table -- 表格模版
      */
     public $ConfigId;
 
@@ -134,8 +150,11 @@ ArticalRecognize -- 手写作文模版
     /**
      * @param string $ImageUrl 图片的 Url 地址。支持的图片格式：PNG、JPG、JPEG，WORD，EXCEL，暂不支持 GIF 格式。支持的图片大小：所下载图片经 Base64 编码后不超过 7M。图片下载时间不超过 3 秒。支持的图片像素：需介于20-10000px之间。图片存储于腾讯云的 Url 可保障更高的下载速度和稳定性，建议图片存储于腾讯云。非腾讯云存储的 Url 速度和稳定性可能受一定影响。
      * @param string $ImageBase64 图片的 Base64 值。支持的图片格式：PNG、JPG、JPEG，WORD，EXCEL，暂不支持 GIF 格式。支持的图片大小：所下载图片经Base64编码后不超过 7M。图片下载时间不超过 3 秒。支持的图片像素：需介于20-10000px之间。图片的 ImageUrl、ImageBase64 必须提供一个，如果都提供，只使用 ImageUrl。
-     * @param integer $PdfPageNumber 需要识别的PDF页面的对应页码，仅支持PDF单页识别，当上传文件为PDF且IsPdf参数值为true时有效，默认值为1。
+     * @param integer $PdfPageNumber 需要识别的PDF页面的对应页码，仅支持PDF单页识别，当上传文件为PDF且IsPdf参数值为true时有效，默认值为前3页。
      * @param array $ItemNames 自定义结构化功能需返回的字段名称，例：若客户想新增返回姓名、性别两个字段的识别结果，则输入ItemNames=["姓名","性别"]
+     * @param boolean $ItemNamesShowMode true：仅输出自定义字段
+flase：输出默认字段+自定义字段
+默认true
      * @param boolean $ReturnFullText 是否开启全文字段识别
      * @param string $ConfigId 配置id支持：
 General -- 通用场景 
@@ -153,6 +172,7 @@ AirWayBill -- 航空运单识别模板
 DispatchWeightNote -- 磅单发货单识别模板
 ReceiptWeightNote -- 磅单收货单识别模板
 ArticalRecognize -- 手写作文模版
+Table -- 表格模版
      * @param boolean $EnableCoord 是否开启全文字段坐标值的识别
      * @param boolean $OutputParentKey 是否开启父子key识别，默认是
      * @param ConfigAdvanced $ConfigAdvanced 模版的单个属性配置
@@ -184,6 +204,10 @@ ArticalRecognize -- 手写作文模版
 
         if (array_key_exists("ItemNames",$param) and $param["ItemNames"] !== null) {
             $this->ItemNames = $param["ItemNames"];
+        }
+
+        if (array_key_exists("ItemNamesShowMode",$param) and $param["ItemNamesShowMode"] !== null) {
+            $this->ItemNamesShowMode = $param["ItemNamesShowMode"];
         }
 
         if (array_key_exists("ReturnFullText",$param) and $param["ReturnFullText"] !== null) {
